c074aa9cc42c42ee4d09e30738ba2d2afc6f8af8
ayadij <ayadijkwel@yahoo.com>
2018-02-17 02:06:59 -0700

    customized routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'pages/home'
 
-  get 'pages/about'
-
-  get 'pages/garage'
+  get 'about', to: 'pages#about'
+  get 'garage', to: 'pages#garage'
 
   resources :cars
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
 end
