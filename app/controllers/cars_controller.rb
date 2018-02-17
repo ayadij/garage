853527c61582e878c7eeea0c0ea16b52725f2c1c
ayadij <ayadijkwel@yahoo.com>
2018-02-17 01:47:10 -0700

    deleted json related stuff generated by the scaffold and made able to run on localhost:3000

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -29,10 +29,8 @@ class CarsController < ApplicationController
     respond_to do |format|
       if @car.save
         format.html { redirect_to @car, notice: 'Car was successfully created.' }
-        format.json { render :show, status: :created, location: @car }
       else
         format.html { render :new }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -43,10 +41,8 @@ class CarsController < ApplicationController
     respond_to do |format|
       if @car.update(car_params)
         format.html { redirect_to @car, notice: 'Car was successfully updated.' }
-        format.json { render :show, status: :ok, location: @car }
       else
         format.html { render :edit }
-        format.json { render json: @car.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -57,7 +53,6 @@ class CarsController < ApplicationController
     @car.destroy
     respond_to do |format|
       format.html { redirect_to cars_url, notice: 'Car was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   get 'pages/garage'
 
   resources :cars
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
